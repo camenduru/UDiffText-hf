f5440deaf681550937481d1071b49ac25c6320db
ZYMPKU <82540602@qq.com>
2023-12-03 15:16:51 +0800

    thtml

--- app.py
+++ app.py
@@ -150,10 +150,10 @@ if __name__ == "__main__":
                 <h1 style="font-weight: 600; font-size: 2rem; margin: 0rem">
                     UDiffText: A Unified Framework for High-quality Text Synthesis in Arbitrary Images via Character-aware Diffusion Models
                 </h1>        
-                <h3 style="font-weight: 450; font-size: 1rem; margin: 0rem"> 
-                    <a href='https://arxiv.org/pdf/******'><img src='https://img.shields.io/badge/Arxiv-******-DF826C'></a> 
-                    <a href='https://github.com/ZYM-PKU/UDiffText'><img src='https://img.shields.io/badge/Code-UDiffText-D0F288'></a> 
-                    <a href='https://udifftext.github.io'><img src='https://img.shields.io/badge/Project-UDiffText-8ADAB2'></a> 
+                <h3 style="font-weight: 450; font-size: 1rem; margin: 0rem; overflow: hidden;"> 
+                    <a style="float: left" href='https://arxiv.org/pdf/******'><img src='https://img.shields.io/badge/Arxiv-******-DF826C'></a> 
+                    <a style="float: left" href='https://github.com/ZYM-PKU/UDiffText'><img src='https://img.shields.io/badge/Code-UDiffText-D0F288'></a> 
+                    <a style="float: left" href='https://udifftext.github.io'><img src='https://img.shields.io/badge/Project-UDiffText-8ADAB2'></a> 
                 </h3> 
                 <h2 style="text-align: left; font-weight: 450; font-size: 1rem; margin-top: 0.5rem; margin-bottom: 0.5rem">
                     Our proposed UDiffText is capable of synthesizing accurate and harmonious text in either synthetic or real-word images, thus can be applied to tasks like scene text editing (a), arbitrary text generation (b) and accurate T2I generation (c)
